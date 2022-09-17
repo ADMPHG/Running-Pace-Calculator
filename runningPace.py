93fde8db88c3871f9ce7a069845cf28c6c84418a
ADMPHG <65092815+ADMPHG@users.noreply.github.com>
2022-09-17 20:44:58 +0100

    Changed program to separate user input into km/m and min/sec from beginning to make calculations and output easier/more versatile.

--- runningPace.py
+++ runningPace.py
@@ -1,36 +1,68 @@
+# splits user input into whole numbers and remainder if it contains a period or colon
+# used by take_distance() and take_time()
+def handle_remainder(userInput):
+    if userInput.find('.') != -1:
+        decomp = userInput.split('.')
+        part1 = decomp[0]
+        part2 = decomp[1]
+        return part1, part2
+
+    elif userInput.find(':') != -1:
+        decomp = userInput.split(':')
+        part1 = decomp[0]
+        part2 = decomp[1]
+        return part1, part2
+    
+    else:
+        return userInput, 0
+
+# takes distance from user, convert decomposed str into ints
+# if anything but ints are entered, user is prompted to input a valid number
+# BUG currently multiple periods/colons are accepted which needs to be addressed
 def take_distance():
     while True:
         try:
-            distance = float(input('Enter the distance you are running (in km): '))
+            distance = input('Enter the distance you are running (in km): ')
+            kilometers, meters = handle_remainder(distance)
+            kilometers = int(kilometers)
+            meters = int(meters)
             break
         except ValueError:
             print('Enter a valid number')
 
-    return distance
+    return kilometers, meters
 
+# takes time from user, convert decomposed str into ints
+# if anything but ints are entered, user is prompted to input a valid number
+# BUG currently multiple periods/colons are accepted which needs to be addressed
 def take_time():
     while True:
         try:
-            time = float(input('Enter the time you would like to finish (in mins): '))
-            time = (time - (time % 1)) + (time % 1) * (1 + (2 / 3))
+            time = input('Enter the time you would like to finish (in mins): ')
+            minutes, seconds = handle_remainder(time)
+            minutes = int(minutes)
+            seconds = int(seconds)
             break
         except ValueError:
             print('Enter a valid number')
-    return time
+    return minutes, seconds
 
-def calc_pace(distance, time):
-    pace = time / distance
-    if pace % 1 != 0:
-        pace = (pace - (pace % 1)) + (pace % 1) * 0.6
-        seconds = (time % 1) * 60
-        minutes = time - (time % 1)
-        print(f'Pace required to run {int(distance)} kilometers in {int(minutes)} minutes and {int(seconds)} seconds is {pace} mins/km')
+# calculates pace from user supplied values - main concern is conversion from 60ths to 100ths and back for seconds
+# may want to create separate function to handle different print statement cases
+def calc_pace(kilometers, meters, minutes, seconds):
+    if seconds != 0 or meters != 0:
+        pace = ((minutes + 0.01 * (seconds * (1 + (2 / 3)))) / (kilometers + 0.1 * (meters))) # recombine minutes/seconds and kilometers/meters
+        paceSec = round(((pace % 1) * 60), 2)
+        paceMins = int(pace - (pace % 1))
+        print(f'Pace required to run {kilometers}.{meters} kilometers in {minutes} minutes and {seconds} seconds is {paceMins}:{paceSec} mins/km')
     else:
-        print(f'Pace required to run {int(distance)} kilometers in {int(time)} minutes is {pace} mins/km')
+        pace = minutes / kilometers
+        pace = (pace - pace % 1) + (pace % 1) * 0.6
+        print(f'Pace required to run {kilometers} kilometers in {minutes} minutes is {pace} mins/km')
 
 def main():
-    distance = take_distance()
-    time = take_time()
-    calc_pace(distance, time)
+    kilometers, meters = take_distance()
+    minutes, seconds = take_time()
+    calc_pace(kilometers, meters, minutes, seconds)
 
 main()
